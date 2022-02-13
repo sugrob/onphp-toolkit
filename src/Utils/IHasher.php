95eb4b91d1e8c9e2dc7716b88fc81f11e9970d02
sugrob <sugrob2008@gmail.com>
2022-02-13 15:33:33 +0300

    add PassHasher

---
+++ src/Utils/IHasher.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace sugrob\OnPHPToolkit\Utils;
+
+interface IHasher {
+	/**
+	 * @param $string
+	 * @return string
+	 */
+	public function hash(string $string):string;
+
+	/**
+	 * @param string $string
+	 * @param string $hash
+	 * @return bool
+	 */
+	public function compare(string $string, string $hash):bool;
+}
---
+++ src/Utils/PassHasher.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace sugrob\OnPHPToolkit\Utils;
+
+class PassHasher implements IHasher
+{
+	const DEFAULT_HASH_POWER = 256; //2^8
+
+	private $salt;
+	private $power;
+
+	public function __construct($salt, $pow = self::DEFAULT_HASH_POWER) {
+		$this->salt = $salt;
+		$this->power = $pow;
+	}
+
+	public function hash(string $string):string {
+		return $this->multiHash($string.$this->salt);
+	}
+
+	public function compare(string $string, string $hash):bool {
+		return $this->hash($string) == $hash;
+	}
+
+	private function multiHash(string $string):string {
+		for ($i = 0; $i < $this->power; $i++) {
+			$string = md5($string);
+		}
+
+		return $string;
+	}
+}
